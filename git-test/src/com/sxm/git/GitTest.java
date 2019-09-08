f873f31b11cd5edace5dea537b6954474ea9fa17
xiaoman.shi <xiaoman.shi@xinchan.com>
2019-09-08 17:29:54 +0800

    one

---
+++ git-test/src/com/sxm/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.sxm.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git test");
+    }
+}
